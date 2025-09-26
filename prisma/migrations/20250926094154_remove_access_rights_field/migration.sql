5e63d167a65f9bd795d68355d6ecdbce96ca79b8
RambleBoy <techodev.2024@gmail.com>
2025-09-26 16:42:47 +0700

    d1

---
+++ prisma/migrations/20250926094154_remove_access_rights_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessRights` on the `cisa_registrations` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."cisa_registrations" DROP COLUMN "accessRights";
